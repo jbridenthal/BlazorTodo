7607eef6a0b08d523e8bc88f6d9f6bd3f66cc813
jbridenthal <jamesbridenthal@gmail.com>
2021-09-24 21:17:16 -0400

    Moved Todos from string to models

--- BlazorTodo/Models/TodoModel.cs
+++ BlazorTodo/Models/TodoModel.cs
@@ -2,7 +2,7 @@
 namespace BlazorTodo.Models;
 public class TodoModel
 {
-    public int ID {  get; set; }
+    public Guid ID {  get; set; }
     public string? Name {  get; set; }
-    public string? Completed {  get; set; }
+    public bool? Completed {  get; set; }
 }
